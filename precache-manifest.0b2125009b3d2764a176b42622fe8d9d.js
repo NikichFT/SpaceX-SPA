0be9dc717708a958cb2418d093292e6c239d2f70
NikichFT <nikich201198@gmail.com>
2020-10-31 03:34:53 +0300

    Updates

---
+++ precache-manifest.0b2125009b3d2764a176b42622fe8d9d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fe423c67bfb64012ad721f7b08999ca",
+    "url": "/SpaceX-SPA/index.html"
+  },
+  {
+    "revision": "67ca9c1ad255c8c78c2f",
+    "url": "/SpaceX-SPA/static/css/main.eab438da.chunk.css"
+  },
+  {
+    "revision": "678c99e80929b374d03d",
+    "url": "/SpaceX-SPA/static/js/2.6374f6a4.chunk.js"
+  },
+  {
+    "revision": "8a9adb48f7af2062183ac70b4922c4ef",
+    "url": "/SpaceX-SPA/static/js/2.6374f6a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67ca9c1ad255c8c78c2f",
+    "url": "/SpaceX-SPA/static/js/main.87a5074b.chunk.js"
+  },
+  {
+    "revision": "836f87d2b1b29d3c211168ceae000f29",
+    "url": "/SpaceX-SPA/static/js/main.87a5074b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41ffa7eda12488de5ab2",
+    "url": "/SpaceX-SPA/static/js/runtime-main.17358838.js"
+  },
+  {
+    "revision": "d5ab31ce033fc418fd1af87f7b934e1b",
+    "url": "/SpaceX-SPA/static/media/logo.d5ab31ce.svg"
+  }
+]);
